3ffbfe16120cc875d71ecdf37d939d6a37f886ee
omeralmali1 <omeralmali1@gmail.com>
2021-02-12 22:11:46 +0300

    Add project files.

---
+++ Payment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Payment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maas, zamOran, zamliMaas;
+            Console.Write("Maaşı girin : ");
+            maas = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Zam oranını girin (%) : ");
+            zamOran = Convert.ToDouble(Console.ReadLine());
+            zamliMaas = maas + (maas * zamOran / 100);
+            Console.WriteLine("Zamlı maaş : {0}", zamliMaas);
+            Console.ReadKey();
+        }
+    }
+}
